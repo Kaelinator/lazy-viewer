67ada6cd3ff389b28a6f3935fb15e1ac94b74a5f
Kaelinator <kirkkael@gmail.com>
2020-03-25 16:46:46 -0500

    added audio analyser

--- content.js
+++ content.js
@@ -1,9 +1,38 @@
 const players = [...document.getElementsByTagName('video')]
 
 const addUI = (video, i) => {
-  const overlay = document.createElement('h1')
-  overlay.innerText = 'hi'
+  const overlay = document.createElement('div')
+  overlay.className = 'video-overlay'
+  const controls = document.createElement('div')
+  controls.className = 'video-controls'
+  const header = document.createElement('h1')
+  header.innerText = 'content'
+  controls.appendChild(header)
+  overlay.appendChild(controls)
+
   video.parentNode.insertBefore(overlay, video)
 }
 
-players.forEach(addUI)
+const getAudio = (video, i) => {
+  // const context = new AudioContext()
+  // const gainNode = context.createGain()
+  // source = context.createMediaElementSource(video)
+  // source.connect(gainNode)
+  const context = new AudioContext()
+  const analyser = context.createAnalyser()
+  analyser.fftSize = 256
+  const dataArray = new Uint8Array(analyser.frequencyBinCount)
+  source = context.createMediaElementSource(video)
+  source.connect(analyser)
+  analyser.connect(context.destination)
+
+  setInterval(() => {
+    analyser.getByteTimeDomainData(dataArray)
+    const sum = dataArray.reduce((s, a) => s + a, 0)
+
+    console.log(sum / dataArray.length)
+  }, 16)
+}
+
+// players.forEach(addUI)
+players.forEach(getAudio)
